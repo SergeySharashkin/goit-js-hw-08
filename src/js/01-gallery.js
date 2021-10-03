661ee8e97f9cd8cd77444c8bf2c018becd6e6f03
SergeySharashkin <S_sharashkin@mail.ru>
2021-10-04 00:32:22 +0300

    вв

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,52 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+  })
+  .join('');
+};
+galleryEl.addEventListener('click', onClick);
+let instance;
+
+function onClick(evt) {
+	evt.preventDefault();
+	if (evt.target.nodeName !== "IMG") {
+		return;
+	}
+	const bigUrl = evt.target.dataset.source;
+	instance = basicLightbox.create(
+		`
+    <img src="${bigUrl}" width="1280">
+`,
+		{
+			onClose: () => {
+				document.removeEventListener("keyup", closeModal);
+			},
+		}
+	);
+	instance.show();
+	document.addEventListener("keyup", closeModal);
+}
+
+function closeModal(evt) {
+	if (evt.code === "Escape") {
+		instance.close();
+	}
+}
