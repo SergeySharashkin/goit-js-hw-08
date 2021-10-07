2160c7fbdeec41a8f5573d42550983d340105fe7
SergeySharashkin <S_sharashkin@mail.ru>
2021-10-08 02:26:28 +0300

    11

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,26 @@
-
 import Player from '@vimeo/player';
-
+// var iframe = document.querySelector('iframe');
+// var player = new Player(iframe);
+//   var player = new Player(iframe, {
 require(['@vimeo/player'], function (Player) {
-        var iframe = document.querySelector('iframe');
-        var player = new Player(iframe);
-
-        player.on('play', function() {
-            console.log('played the video!');
-        });
-    });
+  var iframe = document.querySelector('iframe');
+  var player = new Player(iframe);
+  player.on('play', function () {
+    console.log('played the video!');
+  });
     
-    localStorage.setItem('time', JSON.stringify());
-    const savedTime = localStorage.getItem('time');
-    const parselTime = JSON.parse(savedTime);
+  
+
+//   player.getBuffered().then(function (buffered) {
+//     console.log(buffered);
+//   })
+//   .catch(function (error) {
+//     console.log('an error occurred')
+//   });
+//   player.getCurrentChapter().then(function(chapter) {
+//     console.log(chapter)
+// }).catch(function(error) {
+//     // an error occurred
+// });
+});
+
