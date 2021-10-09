12b98aad4de067a26d7b9439a9892b6c98c79fd1
SergeySharashkin <S_sharashkin@mail.ru>
2021-10-10 02:18:22 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,18 @@
-// // import Player from '@vimeo/player';
-
-// require('@vimeo/player', function (Player) {
-//     const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
-
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
-
-// // player.getVideoTitle().then(function(title) {
-// //     console.log('title:', title);
-// // });
-//   });
-var iframe = document.querySelector('iframe');
-var player = new Vimeo.Player(iframe);
-
-player.on('play', function() {
-    console.log('played the video!');
+var throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const TIME_DATA = 'videoplayer-current-time';
+let savedTime =  localStorage.getItem(TIME_DATA);
+console.log(savedTime);
+player.setCurrentTime(savedTime).then(function(seconds) {
+    return
 });
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
+player.on('timeupdate', throttle(timeCurrenter, 1000));
+function timeCurrenter() {
+      player.getCurrentTime().then(function (seconds) {
+      console.log(seconds);
+      localStorage.setItem(TIME_DATA, seconds);
+    });
+  };
+// player.setCurrentTime(localStorage.setItem(TIME_DATA));
